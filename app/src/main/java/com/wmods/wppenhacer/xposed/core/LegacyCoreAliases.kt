40cd4cf690773acdcdfe93da6051c0c12735c224
Shikateki <radiantdit511@gmail.com>
2026-09-11 14:24:47 +0700

    fix legacy aliases

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/LegacyCoreAliases.kt
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/LegacyCoreAliases.kt
@@ -1,7 +1,5 @@
 package com.wmods.wppenhacer.xposed.core
 
-import android.content.SharedPreferences
-
 /**
  * Compatibility aliases for the legacy EdXposed WppCore source.
  * The imports were intentionally kept out of WppCore while switching the bridge.
@@ -19,10 +17,3 @@ typealias XposedHelpers = de.robv.android.xposed.XposedHelpers
 
 val Utils get() = com.wmods.wppenhacer.xposed.utils.Utils
 val Unobfuscator get() = com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator
-
-/** Local replacement for androidx.core.content.edit used by the legacy WppCore source. */
-inline fun SharedPreferences.edit(commit: Boolean = false, action: SharedPreferences.Editor.() -> Unit) {
-    val editor = edit()
-    editor.action()
-    if (commit) editor.commit() else editor.apply()
-}
